436338a99f0bde2789453598415a3dfbbcdf1f8f
LinnHtet29 <linnh3064@gmail.com>
2024-05-09 18:35:26 +0630

    [add] initial artist

---
+++ src/app/artist/[id]/page.js
@@ -0,0 +1,10 @@
+import ArtistDetail from "@/ui/component/artist/ArtistDetail";
+import React from "react";
+
+export default function page() {
+  return (
+    <section>
+      <ArtistDetail />
+    </section>
+  );
+}
---
+++ src/app/artist/page.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function page() {
+  return <div>Artist page is here</div>;
+}
