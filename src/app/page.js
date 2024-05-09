334819c66afcb3d453990d761b93bc755c35b59a
heinnthantsin <heinnthant01@gmail.com>
2024-05-09 18:34:42 +0630

    content change

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,8 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
      <h1>Welcome Happy Developing...</h1>
+     <h3>Something in the</h3>
   
     </main>
   );
