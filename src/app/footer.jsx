b01e8a53ef10eb85ab249fda36a2c4209ed31c34
Aixin <htethtetwint12.es@gmail.com>
2024-05-10 22:14:30 +0630

    footer

--- src/app/footer.jsx
+++ src/app/footer.jsx
@@ -2,8 +2,12 @@ import React from 'react'
 
 function footer() {
   return (
-    <div>footer
-        Heloo this is footer
+    <div>
+      <div> <img src="logo" alt="Logo Image" /> 
+      <a href=""><i class="fa-brands fa-facebook fa-xl" ></i></a>
+      <a href="https://www.twitter.com/"><i class="fa-brands fa-twitter fa-xl" ></i></a>
+      <a href="https://www.instagram.com/"> <i class="fa-brands fa-instagram fa-xl" ></i></a>
+      </div>
     </div>
   )
 }
